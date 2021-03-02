bd845f3f72b6e7a93d7ecc23ba22548d2fb5d0c1
Limei <48501107+limei0307@users.noreply.github.com>
2021-03-02 15:48:16 -0600

    Update spherenet.py

--- dig/3dgraph/smp/spherenet.py
+++ dig/3dgraph/smp/spherenet.py
@@ -1,1 +1,465 @@
+import os
+import numpy as np
+import torch
+from torch import nn
+from torch.nn import Linear, Embedding
+from torch_geometric.nn.acts import swish
+from torch_geometric.nn.inits import glorot_orthogonal
 
+from torch_scatter import scatter
+import torch.nn.functional as F
+
+from scipy.optimize import brentq
+from scipy import special as sp
+from math import sqrt, pi as PI
+
+try:
+    import sympy as sym
+except ImportError:
+    sym = None
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+class emb_spherenet(torch.nn.Module):
+    def __init__(self, num_radial, num_spherical, cutoff, envelope_exponent):
+        super(emb_spherenet, self).__init__()
+        self.dist_emb = dist_emb_spherenet(num_radial, cutoff, envelope_exponent)
+        self.angle_emb = angle_emb_spherenet(num_spherical, num_radial, cutoff, envelope_exponent)
+        self.torsion_emb = torsion_emb_spherenet(num_spherical, num_radial, cutoff, envelope_exponent)
+        self.reset_parameters()
+    
+    def reset_parameters(self):
+        self.dist_emb.reset_parameters()
+
+    def forward(self, dist, angle, torsion, idx_kj):
+        dist_emb = self.dist_emb(dist)
+        angle_emb = self.angle_emb(dist, angle, idx_kj)
+        torsion_emb = self.torsion_emb(dist, angle, torsion, idx_kj)
+        return dist_emb, angle_emb, torsion_emb
+
+class ResidualLayer(torch.nn.Module):
+    def __init__(self, hidden_channels, act=swish):
+        super(ResidualLayer, self).__init__()
+        self.act = act
+        self.lin1 = Linear(hidden_channels, hidden_channels)
+        self.lin2 = Linear(hidden_channels, hidden_channels)
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        glorot_orthogonal(self.lin1.weight, scale=2.0)
+        self.lin1.bias.data.fill_(0)
+        glorot_orthogonal(self.lin2.weight, scale=2.0)
+        self.lin2.bias.data.fill_(0)
+
+    def forward(self, x):
+        return x + self.act(self.lin2(self.act(self.lin1(x))))
+
+
+class init_spherenet(torch.nn.Module):
+    def __init__(self, num_radial, hidden_channels, act=swish):
+        super(init_spherenet, self).__init__()
+        self.act = act
+        self.emb = Embedding(95, hidden_channels)
+        self.lin_rbf_0 = Linear(num_radial, hidden_channels)
+        self.lin = Linear(3 * hidden_channels, hidden_channels)
+        self.lin_rbf_1 = nn.Linear(num_radial, hidden_channels, bias=False)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.emb.weight.data.uniform_(-sqrt(3), sqrt(3))
+        self.lin_rbf_0.reset_parameters()
+        self.lin.reset_parameters()
+        glorot_orthogonal(self.lin_rbf_1.weight, scale=2.0)
+
+    def forward(self, x, emb, i, j):
+        rbf,_,_ = emb
+        x = self.emb(x)
+        rbf = self.act(self.lin_rbf_0(rbf))
+        e1 = self.act(self.lin(torch.cat([x[i], x[j], rbf], dim=-1)))
+        e2 = self.lin_rbf_1(rbf) * e1
+
+        return e1, e2 #torch.stack([e1,e2],0)
+
+
+class update_e_spherenet(torch.nn.Module):
+    def __init__(self, hidden_channels, int_emb_size, basis_emb_size, num_spherical, num_radial, 
+        num_before_skip, num_after_skip, act=swish):
+        super(update_e_spherenet, self).__init__()
+        self.act = act
+        self.lin_rbf1 = nn.Linear(num_radial, basis_emb_size, bias=False)
+        self.lin_rbf2 = nn.Linear(basis_emb_size, hidden_channels, bias=False)
+        self.lin_sbf1 = nn.Linear(num_spherical * num_radial, basis_emb_size, bias=False)
+        self.lin_sbf2 = nn.Linear(basis_emb_size, int_emb_size, bias=False)
+        self.lin_t1 = nn.Linear(num_spherical * num_spherical * num_radial, basis_emb_size, bias=False) #basis_emb_size num_spherical
+        self.lin_t2 = nn.Linear(basis_emb_size, int_emb_size, bias=False)
+        self.lin_rbf = nn.Linear(num_radial, hidden_channels, bias=False)
+
+        self.lin_kj = nn.Linear(hidden_channels, hidden_channels)
+        self.lin_ji = nn.Linear(hidden_channels, hidden_channels)
+
+        self.lin_down = nn.Linear(hidden_channels, int_emb_size, bias=False)
+        self.lin_up = nn.Linear(int_emb_size, hidden_channels, bias=False)
+
+        self.layers_before_skip = torch.nn.ModuleList([
+            ResidualLayer(hidden_channels, act)
+            for _ in range(num_before_skip)
+        ])
+        self.lin = nn.Linear(hidden_channels, hidden_channels)
+        self.layers_after_skip = torch.nn.ModuleList([
+            ResidualLayer(hidden_channels, act)
+            for _ in range(num_after_skip)
+        ])
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        glorot_orthogonal(self.lin_rbf1.weight, scale=2.0)
+        glorot_orthogonal(self.lin_rbf2.weight, scale=2.0)
+        glorot_orthogonal(self.lin_sbf1.weight, scale=2.0)
+        glorot_orthogonal(self.lin_sbf2.weight, scale=2.0)
+        glorot_orthogonal(self.lin_t1.weight, scale=2.0)
+        glorot_orthogonal(self.lin_t2.weight, scale=2.0)
+
+        glorot_orthogonal(self.lin_kj.weight, scale=2.0)
+        self.lin_kj.bias.data.fill_(0)
+        glorot_orthogonal(self.lin_ji.weight, scale=2.0)
+        self.lin_ji.bias.data.fill_(0)
+
+        glorot_orthogonal(self.lin_down.weight, scale=2.0)
+        glorot_orthogonal(self.lin_up.weight, scale=2.0)
+
+        for res_layer in self.layers_before_skip:
+            res_layer.reset_parameters()
+        glorot_orthogonal(self.lin.weight, scale=2.0)
+        self.lin.bias.data.fill_(0)
+        for res_layer in self.layers_after_skip:
+            res_layer.reset_parameters()
+
+        glorot_orthogonal(self.lin_rbf.weight, scale=2.0)
+
+    def forward(self, e, emb, idx_kj, idx_ji):
+        rbf, sbf, t = emb
+        e1,_ = e
+
+        x_ji = self.act(self.lin_ji(e1))
+        x_kj = self.act(self.lin_kj(e1))
+
+        rbf = self.lin_rbf1(rbf)
+        rbf = self.lin_rbf2(rbf)
+        x_kj = x_kj * rbf
+
+        x_kj = self.act(self.lin_down(x_kj))
+
+        sbf = self.lin_sbf1(sbf)
+        sbf = self.lin_sbf2(sbf)
+        x_kj = x_kj[idx_kj] * sbf
+
+        t = self.lin_t1(t)
+        t = self.lin_t2(t)
+        x_kj = x_kj * t
+
+        x_kj = scatter(x_kj, idx_ji, dim=0, dim_size=x.size(0))
+        x_kj = self.act(self.lin_up(x_kj))
+
+        e1 = x_ji + x_kj
+        for layer in self.layers_before_skip:
+            e1 = layer(e1)
+        e1 = self.act(self.lin(e1)) + x
+        for layer in self.layers_after_skip:
+            e1 = layer(e1)
+        e2 = self.lin_rbf_1(rbf) * e1
+
+        return e1, e2 #torch.stack([e1,e2],0)
+
+
+class update_v_spherenet(torch.nn.Module):
+    def __init__(self, num_radial, hidden_channels, out_emb_channels, out_channels, num_output_layers, act, output_init):
+        super(update_v_spherenet, self).__init__()
+        self.act = act
+        self.output_init = output_init
+
+        self.lin_up = nn.Linear(hidden_channels, out_emb_channels, bias=True)
+        self.lins = torch.nn.ModuleList()
+        for _ in range(num_output_layers):
+            self.lins.append(nn.Linear(out_emb_channels, out_emb_channels))
+        self.lin = nn.Linear(out_emb_channels, out_channels, bias=False)
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        glorot_orthogonal(self.lin_up.weight, scale=2.0)
+        for lin in self.lins:
+            glorot_orthogonal(lin.weight, scale=2.0)
+            lin.bias.data.fill_(0)
+        if self.output_init == 'zeros':
+            self.lin.weight.data.fill_(0)
+        if self.output_init == 'GlorotOrthogonal':
+            glorot_orthogonal(self.lin.weight, scale=2.0)
+
+    def forward(self, e, i, num_nodes=None):
+        _, e2 = e
+        v = scatter(e2, i, dim=0, dim_size=num_nodes)
+        v = self.lin_up(v)
+        for lin in self.lins:
+            v = self.act(lin(v))
+        return self.lin(v)
+
+
+class update_u_spherenet(torch.nn.Module):
+    def __init__(self):
+        super(update_u_spherenet, self).__init__()
+
+    def forward(self, u, v, batch):
+        u += scatter(v, batch, dim=0)
+        return u
+
+
+def Jn(r, n):
+    return np.sqrt(np.pi / (2 * r)) * sp.jv(n + 0.5, r)
+
+
+def Jn_zeros(n, k):
+    zerosj = np.zeros((n, k), dtype='float32')
+    zerosj[0] = np.arange(1, k + 1) * np.pi
+    points = np.arange(1, k + n) * np.pi
+    racines = np.zeros(k + n - 1, dtype='float32')
+    for i in range(1, n):
+        for j in range(k + n - 1 - i):
+            foo = brentq(Jn, points[j], points[j + 1], (i, ))
+            racines[j] = foo
+        points = racines
+        zerosj[i][:k] = racines[:k]
+
+    return zerosj
+
+
+def spherical_bessel_formulas(n):
+    x = sym.symbols('x')
+
+    f = [sym.sin(x) / x]
+    a = sym.sin(x) / x
+    for i in range(1, n):
+        b = sym.diff(a, x) / x
+        f += [sym.simplify(b * (-x)**i)]
+        a = sym.simplify(b)
+    return f
+
+
+def bessel_basis(n, k):
+    zeros = Jn_zeros(n, k)
+    normalizer = []
+    for order in range(n):
+        normalizer_tmp = []
+        for i in range(k):
+            normalizer_tmp += [0.5 * Jn(zeros[order, i], order + 1)**2]
+        normalizer_tmp = 1 / np.array(normalizer_tmp)**0.5
+        normalizer += [normalizer_tmp]
+
+    f = spherical_bessel_formulas(n)
+    x = sym.symbols('x')
+    bess_basis = []
+    for order in range(n):
+        bess_basis_tmp = []
+        for i in range(k):
+            bess_basis_tmp += [
+                sym.simplify(normalizer[order][i] *
+                             f[order].subs(x, zeros[order, i] * x))
+            ]
+        bess_basis += [bess_basis_tmp]
+    return bess_basis
+
+
+def sph_harm_prefactor(k, m):
+    return ((2 * k + 1) * np.math.factorial(k - abs(m)) /
+            (4 * np.pi * np.math.factorial(k + abs(m))))**0.5
+
+
+def associated_legendre_polynomials(k, zero_m_only=True):
+    z = sym.symbols('z')
+    P_l_m = [[0] * (j + 1) for j in range(k)]
+
+    P_l_m[0][0] = 1
+    if k > 0:
+        P_l_m[1][0] = z
+
+        for j in range(2, k):
+            P_l_m[j][0] = sym.simplify(((2 * j - 1) * z * P_l_m[j - 1][0] -
+                                        (j - 1) * P_l_m[j - 2][0]) / j)
+        if not zero_m_only:
+            for i in range(1, k):
+                P_l_m[i][i] = sym.simplify((1 - 2 * i) * P_l_m[i - 1][i - 1])
+                if i + 1 < k:
+                    P_l_m[i + 1][i] = sym.simplify(
+                        (2 * i + 1) * z * P_l_m[i][i])
+                for j in range(i + 2, k):
+                    P_l_m[j][i] = sym.simplify(
+                        ((2 * j - 1) * z * P_l_m[j - 1][i] -
+                         (i + j - 1) * P_l_m[j - 2][i]) / (j - i))
+
+    return P_l_m
+
+
+def real_sph_harm(l, zero_m_only=False, spherical_coordinates=True):
+    pi = np.pi
+    if not zero_m_only:
+        x = sym.symbols('x')
+        y = sym.symbols('y')
+        S_m = [x*0]
+        C_m = [1+0*x]
+        # S_m = [0]
+        # C_m = [1]
+        for i in range(1, l):
+            x = sym.symbols('x')
+            y = sym.symbols('y')
+            S_m += [x*S_m[i-1] + y*C_m[i-1]]
+            C_m += [x*C_m[i-1] - y*S_m[i-1]]
+
+    P_l_m = associated_legendre_polynomials(l, zero_m_only)
+    if spherical_coordinates:
+        theta = sym.symbols('theta')
+        z = sym.symbols('z')
+        for i in range(len(P_l_m)):
+            for j in range(len(P_l_m[i])):
+                if type(P_l_m[i][j]) != int:
+                    P_l_m[i][j] = P_l_m[i][j].subs(z, sym.cos(theta))
+        if not zero_m_only:
+            phi = sym.symbols('phi')
+            for i in range(len(S_m)):
+                S_m[i] = S_m[i].subs(x, sym.sin(
+                    theta)*sym.cos(phi)).subs(y, sym.sin(theta)*sym.sin(phi))
+            for i in range(len(C_m)):
+                C_m[i] = C_m[i].subs(x, sym.sin(
+                    theta)*sym.cos(phi)).subs(y, sym.sin(theta)*sym.sin(phi))
+
+    Y_func_l_m = [['0']*(2*j + 1) for j in range(l)]
+    for i in range(l):
+        Y_func_l_m[i][0] = sym.simplify(sph_harm_prefactor(i, 0) * P_l_m[i][0])
+
+    if not zero_m_only:
+        for i in range(1, l):
+            for j in range(1, i + 1):
+                Y_func_l_m[i][j] = sym.simplify(
+                    2**0.5 * sph_harm_prefactor(i, j) * C_m[j] * P_l_m[i][j])
+        for i in range(1, l):
+            for j in range(1, i + 1):
+                Y_func_l_m[i][-j] = sym.simplify(
+                    2**0.5 * sph_harm_prefactor(i, -j) * S_m[j] * P_l_m[i][j])
+
+    return Y_func_l_m
+
+
+class Envelope(torch.nn.Module):
+    def __init__(self, exponent):
+        super(Envelope, self).__init__()
+        self.p = exponent + 1
+        self.a = -(self.p + 1) * (self.p + 2) / 2
+        self.b = self.p * (self.p + 2)
+        self.c = -self.p * (self.p + 1) / 2
+
+    def forward(self, x):
+        p, a, b, c = self.p, self.a, self.b, self.c
+        x_pow_p0 = x.pow(p - 1)
+        x_pow_p1 = x_pow_p0 * x
+        x_pow_p2 = x_pow_p1 * x
+        return 1. / x + a * x_pow_p0 + b * x_pow_p1 + c * x_pow_p2
+
+
+class dist_emb_spherenet(torch.nn.Module):
+    def __init__(self, num_radial, cutoff=5.0, envelope_exponent=5):
+        super(dist_emb_spherenet, self).__init__()
+        self.cutoff = cutoff
+        self.envelope = Envelope(envelope_exponent)
+
+        self.freq = torch.nn.Parameter(torch.Tensor(num_radial))
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        torch.arange(1, self.freq.numel() + 1, out=self.freq).mul_(PI)
+
+    def forward(self, dist):
+        dist = dist.unsqueeze(-1) / self.cutoff
+        return self.envelope(dist) * (self.freq * dist).sin()
+
+
+class angle_emb_spherenet(torch.nn.Module):
+    def __init__(self, num_spherical, num_radial, cutoff=5.0,
+                 envelope_exponent=5):
+        super(angle_emb_spherenet, self).__init__()
+        assert num_radial <= 64
+        self.num_spherical = num_spherical
+        self.num_radial = num_radial
+        self.cutoff = cutoff
+        # self.envelope = Envelope(envelope_exponent)
+
+        bessel_forms = bessel_basis(num_spherical, num_radial)
+        sph_harm_forms = real_sph_harm(num_spherical)
+        self.sph_funcs = []
+        self.bessel_funcs = []
+
+        x, theta = sym.symbols('x theta')
+        modules = {'sin': torch.sin, 'cos': torch.cos}
+        for i in range(num_spherical):
+            if i == 0:
+                sph1 = sym.lambdify([theta], sph_harm_forms[i][0], modules)(0)
+                self.sph_funcs.append(lambda x: torch.zeros_like(x) + sph1)
+            else:
+                sph = sym.lambdify([theta], sph_harm_forms[i][0], modules)
+                self.sph_funcs.append(sph)
+            for j in range(num_radial):
+                bessel = sym.lambdify([x], bessel_forms[i][j], modules)
+                self.bessel_funcs.append(bessel)
+
+    def forward(self, dist, angle, idx_kj):
+        dist = dist / self.cutoff
+        rbf = torch.stack([f(dist) for f in self.bessel_funcs], dim=1)
+        # rbf = self.envelope(dist).unsqueeze(-1) * rbf
+
+        cbf = torch.stack([f(angle) for f in self.sph_funcs], dim=1)
+
+        n, k = self.num_spherical, self.num_radial
+        out = (rbf[idx_kj].view(-1, n, k) * cbf.view(-1, n, 1)).view(-1, n * k)
+        return out
+
+
+class torsion_emb_spherenet(torch.nn.Module):
+    def __init__(self, num_spherical, num_radial, cutoff=5.0,
+                 envelope_exponent=5):
+        super(tor, self).__init__()
+        assert num_radial <= 64
+        self.num_spherical = num_spherical #
+        self.num_radial = num_radial
+        self.cutoff = cutoff
+        # self.envelope = Envelope(envelope_exponent)
+
+        bessel_forms = bessel_basis(num_spherical, num_radial)
+        sph_harm_forms = real_sph_harm(num_spherical, zero_m_only=False)
+        self.sph_funcs = []
+        self.bessel_funcs = []
+
+        x = sym.symbols('x')
+        theta = sym.symbols('theta')
+        phi = sym.symbols('phi')
+        modules = {'sin': torch.sin, 'cos': torch.cos}
+        for i in range(self.num_spherical):
+            if i == 0:
+                sph1 = sym.lambdify([theta, phi], sph_harm_forms[i][0], modules)
+                self.sph_funcs.append(lambda x, y: torch.zeros_like(x) + torch.zeros_like(y) + sph1(0,0)) #torch.zeros_like(x) + torch.zeros_like(y)
+            else:
+                for k in range(-i, i + 1):
+                    sph = sym.lambdify([theta, phi], sph_harm_forms[i][k+i], modules)
+                    self.sph_funcs.append(sph)
+            for j in range(self.num_radial):
+                bessel = sym.lambdify([x], bessel_forms[i][j], modules)
+                self.bessel_funcs.append(bessel)
+
+    def forward(self, dist, angle, phi, idx_kj):
+        dist = dist / self.cutoff
+        rbf = torch.stack([f(dist) for f in self.bessel_funcs], dim=1)
+        cbf = torch.stack([f(angle, phi) for f in self.sph_funcs], dim=1)
+
+        n, k = self.num_spherical, self.num_radial
+        out = (rbf[idx_kj].view(-1, 1, n, k) * cbf.view(-1, n, n, 1)).view(-1, n * n * k)
+        return out
