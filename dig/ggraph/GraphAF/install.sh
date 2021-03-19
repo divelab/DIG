1bced45526e41f860d5842c64b51e21a0f34d18d
K. YAN <38057488+YKQ98@users.noreply.github.com>
2021-03-19 09:59:44 +0800

    Update install.sh

--- dig/ggraph/GraphAF/install.sh
+++ dig/ggraph/GraphAF/install.sh
@@ -5,4 +5,4 @@ conda activate graphaf
 
 conda install pytorch==1.6.0 torchvision==0.7.0 cudatoolkit=10.1 -c pytorch
 conda install -c conda-forge rdkit
-pip install texttable tdqm cairosvg networkx
+pip install texttable tdqm cairosvg networkx notebook
