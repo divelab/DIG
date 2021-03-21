274f55651863c99f45e6ea8654d3f42db8d8ecae
limei0307 <limei@tamu.edu>
2021-03-20 23:37:34 -0500

    Add schnet

---
+++ dig/3dgraph/schnet/README.md
@@ -0,0 +1,40 @@
+# SchNet
+
+This is an re-implementation for [SchNet: A continuous-filter convolutional neural net work for modeling quantum interactions](https://arxiv.org/abs/1706.08566) under the 3DGN framewrok.
+
+## Table of Contents
+
+1. [Setup](#setup)
+1. [Usage](#usage)
+1. [Citation](#citation)
+1. [Acknowledgement](#acknowledgement)
+
+
+## Setup
+
+To install the conda virtual environment `schnet`:
+```shell script
+$ cd /3dgraph/schnet
+$ bash setup.sh
+```
+Note that we use CUDA 10.1 in this project. If you have other CUDA versions, you should install the PyTorch and cudatoolkit compatible with your CUDA. Note that the versions of PyTorch and PyTorch Geometric should be compatible. It would be easy to install PyTorch Geometric by following the [installation instruction](https://pytorch-geometric.readthedocs.io/en/latest/notes/installation.html#).
+
+
+## Usage
+```shell script
+cd 3dgraph/schnet
+CUDA_VISIBLE_DEVICES=${your_gpu_id} python main.py --save_dir $save_dir --dataset $dataset --target $target --batch_size $batch_size --epochs $epochs 
+```
+## Citations
+
+```
+@article{schutt2017schnet,
+  title={Schnet: A continuous-filter convolutional neural network for modeling quantum interactions},
+  author={Sch{\"u}tt, Kristof T and Kindermans, Pieter-Jan and Sauceda, Huziel E and Chmiela, Stefan and Tkatchenko, Alexandre and M{\"u}ller, Klaus-Robert},
+  journal={arXiv preprint arXiv:1706.08566},
+  year={2017}
+}
+```
+
+## Acknowledgement
+Our implementation is based on [Models in PyTorch Geometric](https://github.com/rusty1s/pytorch_geometric/blob/master/torch_geometric/nn/models/schnet.py). Thanks a lot for their awesome works.
---
+++ dig/3dgraph/schnet/main.py
@@ -0,0 +1,66 @@
+import torch
+import time
+import numpy as np
+from schnet import schnet
+import argparse
+import torch.nn.functional as F
+from train import run
+import sys
+sys.path.append('..')'
+from utils import load_qm9, load_md17
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--dataset', type=str, default="qm9", help='dataset name') # qm9, aspirin, benzene, ethanol, malonaldehyde, naphthalene, salicylic, toluene, uracil
+parser.add_argument('--target', type=str, default='U0')
+parser.add_argument('--num_task', type=int, default=1)
+
+parser.add_argument('--save_dir', type=str, default='../trained_models/', help='directory to save the model with best validation performance')
+parser.add_argument('--log_dir', type=str, default='../logs/')
+
+parser.add_argument('--batch_size', type=int, default=32)
+parser.add_argument('--epochs', type=int, default=300)
+parser.add_argument('--lr', type=float, default=0.0005)
+parser.add_argument('--lr_decay_factor', type=float, default=0.5)
+parser.add_argument('--lr_decay_step_size', type=int, default=50)
+parser.add_argument('--weight_decay', type=float, default=0)
+
+parser.add_argument('--energy_and_force', type=bool, default=False)
+parser.add_argument('--num_atom', type=int, default=1)
+parser.add_argument('--p', type=int, default=100)
+
+parser.add_argument('--cutoff', type=float, default=10.0)
+parser.add_argument('--num_layers', type=int, default=6)
+parser.add_argument('--hidden_channels', type=int, default=128)
+parser.add_argument('--num_filters', type=int, default=128)
+parser.add_argument('--num_gaussians', type=int, default=50)
+
+args = parser.parse_args()
+print('Loading data')
+t_start = time.perf_counter()
+if args.dataset == 'qm9':
+    train_size = 110000
+    val_size = 10000
+    train_dataset, val_dataset, test_dataset = load_qm9(args.dataset, args.target, train_size, val_size)
+    t_end = time.perf_counter()
+    print('time',t_end-t_start)
+    print('Loaded data')
+
+elif args.dataset in ['aspirin', 'benzene', 'ethanol', 'malonaldehyde', 'naphthalene', 'salicylic', 'toluene', 'uracil']: #md17
+    train_size = 1000
+    val_size = 10000
+    train_dataset, val_dataset, test_dataset, num_atom = load_md17(args.dataset, train_size, val_size)
+    args.energy_and_force = True
+    args.num_atom = num_atom
+    t_end = time.perf_counter()
+    print('time',t_end-t_start)
+    print('Loaded data')
+
+else:
+    print("This dataset name is not supported!")
+
+print(args)
+print('Loading model')
+model = schnet(args.energy_and_force, args.cutoff, args.num_layers, args.hidden_channels, args.num_filters, args.num_gaussians)
+print('Loaded model')
+run(train_dataset, val_dataset, test_dataset, args.save_dir, args.log_dir, model, args.epochs, args.batch_size, args.lr, args.lr_decay_factor, args.lr_decay_step_size, args.weight_decay, args.energy_and_force, args.num_atom, args.p)
