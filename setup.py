033674b6cb932990fd595bedc2a467caf61691a6
citrine <citrinegui@gmail.com>
2021-04-05 15:19:47 +0800

    Create setup.py: Add basic setting

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dig",
+    version="0.0.1",
+    author="***",
+    author_email="***",
+    # entry_points={
+    #     'console_scripts': [
+    #         'dig=dig.xxx.xxx'
+    #     ]
+    # },
+    description="Dive into Graphs is a turnkey library for graph deep learning research.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/divelab/DIG",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # install_requires=[''],
+    python_requires='>=3.6',
+)
