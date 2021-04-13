a437dfd80b0196cb1795da38e0efbfd85711414c
Oceanusity <1161283769@qq.com>
2021-04-14 03:53:57 +0800

    xgraph: add datasets

---
+++ dig/xgraph/dataset/mol_dataset.py
@@ -0,0 +1,80 @@
+import os
+import torch
+import numpy as np
+from torch_geometric.datasets import MoleculeNet
+from torch_geometric.utils import dense_to_sparse
+from torch_geometric.data import Data, InMemoryDataset, DataLoader
+
+
+class MUTAGDataset(InMemoryDataset):
+    def __init__(self, root, name, transform=None, pre_transform=None):
+        self.root = root
+        self.name = name.upper()
+        super(MUTAGDataset, self).__init__(root, transform, pre_transform)
+        self.data, self.slices = torch.load(self.processed_paths[0])
+
+    def __len__(self):
+        return len(self.slices['x']) - 1
+
+    @property
+    def raw_dir(self):
+        return os.path.join(self.root, self.name, 'raw')
+
+    @property
+    def raw_file_names(self):
+        return ['MUTAG_A', 'MUTAG_graph_labels', 'MUTAG_graph_indicator', 'MUTAG_node_labels']
+
+    @property
+    def processed_dir(self):
+        return os.path.join(self.root, self.name, 'processed')
+
+    @property
+    def processed_file_names(self):
+        return ['data.pt']
+
+    def process(self):
+        r"""Processes the dataset to the :obj:`self.processed_dir` folder."""
+        with open(os.path.join(self.raw_dir, 'MUTAG_node_labels.txt'), 'r') as f:
+            nodes_all_temp = f.read().splitlines()
+            nodes_all = [int(i) for i in nodes_all_temp]
+
+        adj_all = np.zeros((len(nodes_all), len(nodes_all)))
+        with open(os.path.join(self.raw_dir, 'MUTAG_A.txt'), 'r') as f:
+            adj_list = f.read().splitlines()
+        for item in adj_list:
+            lr = item.split(', ')
+            l = int(lr[0])
+            r = int(lr[1])
+            adj_all[l - 1, r - 1] = 1
+
+        with open(os.path.join(self.raw_dir, 'MUTAG_graph_indicator.txt'), 'r') as f:
+            graph_indicator_temp = f.read().splitlines()
+            graph_indicator = [int(i) for i in graph_indicator_temp]
+            graph_indicator = np.array(graph_indicator)
+
+        with open(os.path.join(self.raw_dir, 'MUTAG_graph_labels.txt'), 'r') as f:
+            graph_labels_temp = f.read().splitlines()
+            graph_labels = [int(i) for i in graph_labels_temp]
+
+        data_list = []
+        for i in range(1, 189):
+            idx = np.where(graph_indicator == i)
+            graph_len = len(idx[0])
+            adj = adj_all[idx[0][0]:idx[0][0] + graph_len, idx[0][0]:idx[0][0] + graph_len]
+            label = int(graph_labels[i - 1] == 1)
+            feature = nodes_all[idx[0][0]:idx[0][0] + graph_len]
+            nb_clss = 7
+            targets = np.array(feature).reshape(-1)
+            one_hot_feature = np.eye(nb_clss)[targets]
+            data_example = Data(x=torch.from_numpy(one_hot_feature).float(),
+                                edge_index=dense_to_sparse(torch.from_numpy(adj))[0],
+                                y=label)
+            data_list.append(data_example)
+
+        torch.save(self.collate(data_list), self.processed_paths[0])
+
+
+class MoleculeDataset(MoleculeNet):
+    def __init__(self, root, name, transform=None, pre_transform=None):
+        super(MoleculeDataset, self).__init__(root, name, transform=transform, pre_transform=pre_transform)
+
