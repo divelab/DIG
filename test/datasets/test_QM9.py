4f0c5ef9dd4e7b03dc40e05383917d6ba3a88208
Cong Fu <ffucong@gmail.com>
2021-05-05 15:33:55 -0500

    test qm9

--- test/datasets/test_QM9.py
+++ test/datasets/test_QM9.py
@@ -1,4 +1,5 @@
 from dig.ggraph.dataset import ggraph_dataset
 
-dataset = ggraph_dataset.QM9(root='./dataset', prop_name='penalized_logp')
-assert len(dataset) == 133885
+def test_qm9():
+    dataset = ggraph_dataset.QM9(root='./dataset', prop_name='penalized_logp')
+    assert len(dataset) == 133885
