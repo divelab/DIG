8f2c292f5a4718bc0ec39d359cd355cd4ba31cf4
Yaochen Xie <xieyc95@gmail.com>
2021-05-10 13:05:47 -0500

    Update test

--- test/sslgraph/evaluation/test_NodeUnsupervised.py
+++ test/sslgraph/evaluation/test_NodeUnsupervised.py
@@ -14,7 +14,7 @@ def test_NodeUnsupervised():
                 maskN_rate_1=0.3, maskN_rate_2=0.4, tau=0.4)
     
     evaluator = NodeUnsupervised(dataset, log_interval=1)
-    evaluator.setup_train_config(p_lr=0.0005, p_epoch=10, p_weight_decay=1e-5, comp_embed_on='cpu')
+    evaluator.setup_train_config(p_lr=0.0005, p_epoch=1, p_weight_decay=1e-5, comp_embed_on='cpu')
     test_mean = evaluator.evaluate(learning_model=grace, encoder=encoder)
 
     assert test_mean <= 1.0 and test_mean >= 0.0
