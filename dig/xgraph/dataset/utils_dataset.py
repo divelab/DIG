8f47f1a12f0b1d5c3abdfed329a286a013318ae1
Oceanusity <1161283769@qq.com>
2021-08-04 11:25:10 -0500

    xgraph: add utils_dataste

---
+++ dig/xgraph/dataset/utils_dataset.py
@@ -0,0 +1,26 @@
+import torch
+from torch_geometric.data import Data, Dataset
+
+
+class MarginalSubgraphDataset(Dataset):
+    """ Collect pair-wise graph data to calculate marginal contribution. """
+    def __init__(self, data, exclude_mask, include_mask, subgraph_build_func) -> object:
+        self.num_nodes = data.num_nodes
+        self.X = data.x
+        self.edge_index = data.edge_index
+        self.device = self.X.device
+
+        self.label = data.y
+        self.exclude_mask = torch.tensor(exclude_mask).type(torch.float32).to(self.device)
+        self.include_mask = torch.tensor(include_mask).type(torch.float32).to(self.device)
+        self.subgraph_build_func = subgraph_build_func
+
+    def __len__(self):
+        return self.exclude_mask.shape[0]
+
+    def __getitem__(self, idx):
+        exclude_graph_X, exclude_graph_edge_index = self.subgraph_build_func(self.X, self.edge_index, self.exclude_mask[idx])
+        include_graph_X, include_graph_edge_index = self.subgraph_build_func(self.X, self.edge_index, self.include_mask[idx])
+        exclude_data = Data(x=exclude_graph_X, edge_index=exclude_graph_edge_index)
+        include_data = Data(x=include_graph_X, edge_index=include_graph_edge_index)
+        return exclude_data, include_data
