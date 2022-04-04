5c38df481b56c2f487c2fcf303beb4974a41ed24
semihcanturk <canturks@seas.upenn.edu>
2022-04-04 16:56:38 +0300

    add GTaxoGym as submodule, test BandpassFiltering and WaveletBankFiltering in example_graphcl_NCI1.ipynb

--- dig/sslgraph/method/contrastive/model/graphcl.py
+++ dig/sslgraph/method/contrastive/model/graphcl.py
@@ -1,6 +1,7 @@
 from .contrastive import Contrastive
 from dig.sslgraph.method.contrastive.views_fn import NodeAttrMask, EdgePerturbation, \
     UniformSample, RWSample, RandomView
+from gtaxo_graphgym.transform.perturbations.spectral import BandpassFiltering, WaveletBankFiltering, FiedlerFragmentation
 
 
 class GraphCL(Contrastive):
@@ -23,50 +24,52 @@ class GraphCL(Contrastive):
         aug_ratio (float, optional): The ratio of augmentations. A number between [0,1).
         **kwargs (optinal): Additional arguments of :class:`dig.sslgraph.method.Contrastive`.
     """
-    
+
     def __init__(self, dim, aug_1=None, aug_2=None, aug_ratio=0.2, **kwargs):
-        
+
         views_fn = []
-        
+        aug_dict = {
+            'dropN': UniformSample(ratio=aug_ratio),
+            'permE': EdgePerturbation(ratio=aug_ratio),
+            'subgraph': RWSample(ratio=aug_ratio),
+            'maskN': NodeAttrMask(mask_ratio=aug_ratio),
+            'random2': RandomView([UniformSample(ratio=aug_ratio),
+                                   RWSample(ratio=aug_ratio)]),
+            'random3': RandomView([UniformSample(ratio=aug_ratio),
+                                   RWSample(ratio=aug_ratio),
+                                   EdgePerturbation(ratio=aug_ratio),
+                                   NodeAttrMask(mask_ratio=aug_ratio)]),
+            'random4': RandomView([UniformSample(ratio=aug_ratio),
+                                   RWSample(ratio=aug_ratio),
+                                   EdgePerturbation(ratio=aug_ratio)]),
+            'BPhi': BandpassFiltering(band='hi'),
+            'BPmid': BandpassFiltering(band='mid'),
+            'BPlo': BandpassFiltering(band='lo'),
+            'WBhi': WaveletBankFiltering(bands=[True, False, False]),
+            'WBmid': WaveletBankFiltering(bands=[False, True, False]),
+            'WBlo': WaveletBankFiltering(bands=[False, False, True]),
+            'Fiedler': FiedlerFragmentation()
+        }
+
         for aug in [aug_1, aug_2]:
             if aug is None:
                 views_fn.append(lambda x: x)
-            elif aug == 'dropN':
-                views_fn.append(UniformSample(ratio=aug_ratio))
-            elif aug == 'permE':
-                views_fn.append(EdgePerturbation(ratio=aug_ratio))
-            elif aug == 'subgraph':
-                views_fn.append(RWSample(ratio=aug_ratio))
-            elif aug == 'maskN':
-                views_fn.append(NodeAttrMask(mask_ratio=aug_ratio))
-            elif aug == 'random2':
-                canditates = [UniformSample(ratio=aug_ratio),
-                              RWSample(ratio=aug_ratio)]
-                views_fn.append(RandomView(canditates))
-            elif aug == 'random4':
-                canditates = [UniformSample(ratio=aug_ratio),
-                              RWSample(ratio=aug_ratio),
-                              EdgePerturbation(ratio=aug_ratio)]
-                views_fn.append(RandomView(canditates))
-            elif aug == 'random3':
-                canditates = [UniformSample(ratio=aug_ratio),
-                              RWSample(ratio=aug_ratio),
-                              EdgePerturbation(ratio=aug_ratio),
-                              NodeAttrMask(mask_ratio=aug_ratio)]
-                views_fn.append(RandomView(canditates))
             else:
-                raise Exception("Aug must be from [dropN', 'permE', 'subgraph', \
+                try:
+                    views_fn.append(aug_dict[aug])
+                except:
+                    raise Exception("Aug must be from [dropN', 'permE', 'subgraph', \
                                 'maskN', 'random2', 'random3', 'random4'] or None.")
-        
+
         super(GraphCL, self).__init__(objective='NCE',
                                       views_fn=views_fn,
                                       z_dim=dim,
                                       proj='MLP',
                                       node_level=False,
                                       **kwargs)
-        
+
     def train(self, encoders, data_loader, optimizer, epochs, per_epoch_out=False):
         # GraphCL removes projection heads after pre-training
-        for enc, proj in super(GraphCL, self).train(encoders, data_loader, 
+        for enc, proj in super(GraphCL, self).train(encoders, data_loader,
                                                     optimizer, epochs, per_epoch_out):
             yield enc
