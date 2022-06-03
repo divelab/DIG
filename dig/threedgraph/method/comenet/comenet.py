f303d2edd3be60e5de286721e1301b698990b133
kruskallin <kruskallin@tamu.edu>
2022-06-03 16:02:46 -0500

    update comenet codes for MD17/QM9/OC20

--- dig/threedgraph/method/comenet/comenet.py
+++ dig/threedgraph/method/comenet/comenet.py
@@ -3,7 +3,7 @@ from torch_geometric.nn import GraphConv, GraphNorm
 from torch_geometric.nn.acts import swish
 from torch_geometric.nn import inits
 
-from .features import angle_emb, torsion_emb, RadialBasis
+from .features import angle_emb, torsion_emb
 
 from torch_scatter import scatter, scatter_min
 
@@ -142,7 +142,6 @@ class SimpleInteractionBlock(torch.nn.Module):
             output_channels,
             act=swish,
             inits='glorot',
-            use_angle_feature=True,
     ):
         super(SimpleInteractionBlock, self).__init__()
         self.act = act
@@ -160,12 +159,8 @@ class SimpleInteractionBlock(torch.nn.Module):
         self.norm = GraphNorm(hidden_channels)
 
         # Transformations of Bessel and spherical basis representations.
-        if use_angle_feature:
-            self.lin_feature1 = TwoLayerLinear(num_radial * num_spherical ** 2, hidden_channels, hidden_channels)
-            self.lin_feature2 = TwoLayerLinear(num_radial * num_spherical, hidden_channels, hidden_channels)
-        else:
-            self.lin_feature1 = TwoLayerLinear(num_radial, hidden_channels, hidden_channels)
-            self.lin_feature2 = TwoLayerLinear(num_radial, hidden_channels, hidden_channels)
+        self.lin_feature1 = TwoLayerLinear(num_radial * num_spherical ** 2, hidden_channels, hidden_channels)
+        self.lin_feature2 = TwoLayerLinear(num_radial * num_spherical, hidden_channels, hidden_channels)
 
         # Dense transformations of input messages.
         self.lin = Linear(hidden_channels, hidden_channels)
@@ -229,13 +224,11 @@ class ComENet(nn.Module):
             num_spherical=2,
             cutoff=8.0,
             num_output_layers=3,
-            use_angle_feature=True,
     ):
         super(ComENet, self).__init__()
         self.num_targets = num_targets
         self.cutoff = cutoff
         self.num_blocks = num_blocks
-        self.use_angle_feature = use_angle_feature
 
         if sym is None:
             raise ImportError("Package `sympy` could not be found.")
@@ -243,15 +236,8 @@ class ComENet(nn.Module):
         act = swish
         self.act = act
 
-        if use_angle_feature:
-            self.feature1 = torsion_emb(num_radial=num_radial, num_spherical=num_spherical, cutoff=cutoff)
-            self.feature2 = angle_emb(num_radial=num_radial, num_spherical=num_spherical, cutoff=cutoff)
-        else:
-            self.dist_emb = RadialBasis(
-                num_radial=num_radial,
-                cutoff=cutoff,
-                rbf={"name": "bernstein"}
-            )
+        self.feature1 = torsion_emb(num_radial=num_radial, num_spherical=num_spherical, cutoff=cutoff)
+        self.feature2 = angle_emb(num_radial=num_radial, num_spherical=num_spherical, cutoff=cutoff)
 
         self.emb = EmbeddingBlock(hidden_channels, act)
 
@@ -264,7 +250,6 @@ class ComENet(nn.Module):
                     num_output_layers,
                     hidden_channels,
                     act,
-                    use_angle_feature=use_angle_feature
                 )
                 for _ in range(num_blocks)
             ]
@@ -299,97 +284,92 @@ class ComENet(nn.Module):
         # Embedding block.
         x = self.emb(z)
 
-        if self.use_angle_feature:
-            # Calculate distances.
-            _, argmin0 = scatter_min(dist, i, dim_size=num_nodes)
-            argmin0[argmin0 >= len(i)] = 0
-            n0 = j[argmin0]
-            add = torch.zeros_like(dist).to(dist.device)
-            add[argmin0] = self.cutoff
-            dist1 = dist + add
+        # Calculate distances.
+        _, argmin0 = scatter_min(dist, i, dim_size=num_nodes)
+        argmin0[argmin0 >= len(i)] = 0
+        n0 = j[argmin0]
+        add = torch.zeros_like(dist).to(dist.device)
+        add[argmin0] = self.cutoff
+        dist1 = dist + add
 
-            _, argmin1 = scatter_min(dist1, i, dim_size=num_nodes)
-            argmin1[argmin1 >= len(i)] = 0
-            n1 = j[argmin1]
-            # --------------------------------------------------------
+        _, argmin1 = scatter_min(dist1, i, dim_size=num_nodes)
+        argmin1[argmin1 >= len(i)] = 0
+        n1 = j[argmin1]
+        # --------------------------------------------------------
 
-            _, argmin0_j = scatter_min(dist, j, dim_size=num_nodes)
-            argmin0_j[argmin0_j >= len(j)] = 0
-            n0_j = i[argmin0_j]
+        _, argmin0_j = scatter_min(dist, j, dim_size=num_nodes)
+        argmin0_j[argmin0_j >= len(j)] = 0
+        n0_j = i[argmin0_j]
 
-            add_j = torch.zeros_like(dist).to(dist.device)
-            add_j[argmin0_j] = self.cutoff
-            dist1_j = dist + add_j
+        add_j = torch.zeros_like(dist).to(dist.device)
+        add_j[argmin0_j] = self.cutoff
+        dist1_j = dist + add_j
 
-            # i[argmin] = range(0, num_nodes)
-            _, argmin1_j = scatter_min(dist1_j, j, dim_size=num_nodes)
-            argmin1_j[argmin1_j >= len(j)] = 0
-            n1_j = i[argmin1_j]
+        # i[argmin] = range(0, num_nodes)
+        _, argmin1_j = scatter_min(dist1_j, j, dim_size=num_nodes)
+        argmin1_j[argmin1_j >= len(j)] = 0
+        n1_j = i[argmin1_j]
 
-            # ----------------------------------------------------------
+        # ----------------------------------------------------------
 
-            # n0, n1 for i
-            n0 = n0[i]
-            n1 = n1[i]
+        # n0, n1 for i
+        n0 = n0[i]
+        n1 = n1[i]
 
-            # n0, n1 for j
-            n0_j = n0_j[j]
-            n1_j = n1_j[j]
+        # n0, n1 for j
+        n0_j = n0_j[j]
+        n1_j = n1_j[j]
 
-            # tau: (iref, i, j, jref)
-            # when compute tau, do not use n0, n0_j as ref for i and j,
-            # because if n0 = j, or n0_j = i, the computed tau is zero
-            # so if n0 = j, we choose iref = n1
-            # if n0_j = i, we choose jref = n1_j
-            mask_iref = n0 == j
-            iref = torch.clone(n0)
-            iref[mask_iref] = n1[mask_iref]
-            idx_iref = argmin0[i]
-            idx_iref[mask_iref] = argmin1[i][mask_iref]
+        # tau: (iref, i, j, jref)
+        # when compute tau, do not use n0, n0_j as ref for i and j,
+        # because if n0 = j, or n0_j = i, the computed tau is zero
+        # so if n0 = j, we choose iref = n1
+        # if n0_j = i, we choose jref = n1_j
+        mask_iref = n0 == j
+        iref = torch.clone(n0)
+        iref[mask_iref] = n1[mask_iref]
+        idx_iref = argmin0[i]
+        idx_iref[mask_iref] = argmin1[i][mask_iref]
 
-            mask_jref = n0_j == i
-            jref = torch.clone(n0_j)
-            jref[mask_jref] = n1_j[mask_jref]
-            idx_jref = argmin0_j[j]
-            idx_jref[mask_jref] = argmin1_j[j][mask_jref]
+        mask_jref = n0_j == i
+        jref = torch.clone(n0_j)
+        jref[mask_jref] = n1_j[mask_jref]
+        idx_jref = argmin0_j[j]
+        idx_jref[mask_jref] = argmin1_j[j][mask_jref]
 
-            pos_ji, pos_in0, pos_in1, pos_iref, pos_jref_j = (
-                vecs,
-                vecs[argmin0][i],
-                vecs[argmin1][i],
-                vecs[idx_iref],
-                vecs[idx_jref]
-            )
+        pos_ji, pos_in0, pos_in1, pos_iref, pos_jref_j = (
+            vecs,
+            vecs[argmin0][i],
+            vecs[argmin1][i],
+            vecs[idx_iref],
+            vecs[idx_jref]
+        )
 
-            # Calculate angles.
-            a = ((-pos_ji) * pos_in0).sum(dim=-1)
-            b = torch.cross(-pos_ji, pos_in0).norm(dim=-1)
-            theta = torch.atan2(b, a)
-            theta[theta < 0] = theta[theta < 0] + math.pi
+        # Calculate angles.
+        a = ((-pos_ji) * pos_in0).sum(dim=-1)
+        b = torch.cross(-pos_ji, pos_in0).norm(dim=-1)
+        theta = torch.atan2(b, a)
+        theta[theta < 0] = theta[theta < 0] + math.pi
 
-            # Calculate torsions.
-            dist_ji = pos_ji.pow(2).sum(dim=-1).sqrt()
-            plane1 = torch.cross(-pos_ji, pos_in0)
-            plane2 = torch.cross(-pos_ji, pos_in1)
-            a = (plane1 * plane2).sum(dim=-1)  # cos_angle * |plane1| * |plane2|
-            b = (torch.cross(plane1, plane2) * pos_ji).sum(dim=-1) / dist_ji
-            phi = torch.atan2(b, a)
-            phi[phi < 0] = phi[phi < 0] + math.pi
+        # Calculate torsions.
+        dist_ji = pos_ji.pow(2).sum(dim=-1).sqrt()
+        plane1 = torch.cross(-pos_ji, pos_in0)
+        plane2 = torch.cross(-pos_ji, pos_in1)
+        a = (plane1 * plane2).sum(dim=-1)  # cos_angle * |plane1| * |plane2|
+        b = (torch.cross(plane1, plane2) * pos_ji).sum(dim=-1) / dist_ji
+        phi = torch.atan2(b, a)
+        phi[phi < 0] = phi[phi < 0] + math.pi
 
-            # Calculate right torsions.
-            plane1 = torch.cross(pos_ji, pos_jref_j)
-            plane2 = torch.cross(pos_ji, pos_iref)
-            a = (plane1 * plane2).sum(dim=-1)  # cos_angle * |plane1| * |plane2|
-            b = (torch.cross(plane1, plane2) * pos_ji).sum(dim=-1) / dist_ji
-            tau = torch.atan2(b, a)
-            tau[tau < 0] = tau[tau < 0] + math.pi
+        # Calculate right torsions.
+        plane1 = torch.cross(pos_ji, pos_jref_j)
+        plane2 = torch.cross(pos_ji, pos_iref)
+        a = (plane1 * plane2).sum(dim=-1)  # cos_angle * |plane1| * |plane2|
+        b = (torch.cross(plane1, plane2) * pos_ji).sum(dim=-1) / dist_ji
+        tau = torch.atan2(b, a)
+        tau[tau < 0] = tau[tau < 0] + math.pi
 
-            feature1 = self.feature1(dist, theta, phi)
-            feature2 = self.feature2(dist, tau)
-        else:
-            d_feature = self.dist_emb(dist)
-            feature1 = d_feature
-            feature2 = d_feature
+        feature1 = self.feature1(dist, theta, phi)
+        feature2 = self.feature2(dist, tau)
 
         # Interaction blocks.
         for interaction_block in self.interaction_blocks:
