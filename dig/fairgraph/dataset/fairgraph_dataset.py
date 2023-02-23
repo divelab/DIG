15616d749aced8e3e06f4cbbb501a9223abd2d93
hydra324 <akhilchilumuru@gmail.com>
2023-02-23 12:54:49 -0600

    refactor: updated Pokec in memory dataset class - data now reads from csv file

--- dig/fairgraph/dataset/fairgraph_dataset.py
+++ dig/fairgraph/dataset/fairgraph_dataset.py
@@ -39,141 +39,73 @@ class POKEC(InMemoryDataset):
         if root is None:
             root = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
 
-        self.node_data_map = None
-        """ Maps the original label to a vector."""
-
         self.node_index_map = None
         """ Maps the original key to a node index."""
-
-        self.edge_data_map = None
-        """Maps the original label to a vector."""
 
         self.node_frame = None
         """The original nodes frame."""
 
         self.edge_frame = None
         """The original edges frame."""
+        
         super(POKEC, self).__init__(root, transform, pre_transform, pre_filter)
-        self.data, self.slices = torch.load(self.processed_paths[0])
+        self.data, self.slices = torch.load(self.processed_paths[1])
     
     @property
     def raw_file_names(self):
-        return ["soc-pokec-profiles.txt.gz", "soc-pokec-relationships.txt.gz"]
+        return ["region_job.csv","region_job_relationship.txt","region_job.embedding"]
 
     @property
     def processed_file_names(self):
-        return ["pokec.pt"]
+        return ["data_frames.h5","pokec.pt"]
 
     def download(self):
         # Download to `self.raw_dir`.
-        download_url("https://snap.stanford.edu/data/soc-pokec-profiles.txt.gz", self.raw_dir)
-        download_url("https://snap.stanford.edu/data/soc-pokec-relationships.txt.gz", self.raw_dir)
+        # temporarlity retreiving dataset csv file path from environment var
+        # ideally should be fetched from DIG storage repository
+        download_url(os.environ('POKEC__REGION_JOB'),self.raw_dir)
+        download_url(os.environ('POKEC__REGION_JOB_EMBEDDING'),self.raw_dir)
+        download_url(os.environ('POKEC__REGION_JOB_RELATIONSHIOP'),self.raw_dir)
 
-    def load_frames(self):
+    def read_graph(self):
         """
-        Returns the Pandas node and edge frames.
+        Returns Pandas node and edge dataframes.
         """
-        print("Loading data frames")
-        hdf_path = os.path.join(self.processed_dir, "frames.h5")
+        hdf_path = os.path.join(self.processed_dir, self.processed_paths[0])
         if os.path.exists(hdf_path):
-            self.node_frame = pd.read_hdf(hdf_path, "/dfn")
-            self.edge_frame = pd.read_hdf(hdf_path, "/dfe")
+            self.node_frame = pd.read_hdf(hdf_path, "/df_node")
+            self.edge_frame = pd.read_hdf(hdf_path, "/df_edge")
             return self.node_frame, self.edge_frame
-        node_fields = [
-            "public",
-            "completion_percentage",
-            "gender",
-            "region",
-            "last_login",
-            "registration",
-            "AGE",
-            "body",
-            "I_am_working_in_field",
-            "spoken_languages",
-            "hobbies",
-            "I_most_enjoy_good_food",
-            "pets",
-            "body_type",
-            "my_eyesight",
-            "eye_color",
-            "hair_color",
-            "hair_type",
-            "completed_level_of_education",
-            "favourite_color",
-            "relation_to_smoking",
-            "relation_to_alcohol",
-            "sign_in_zodiac",
-            "on_pokec_i_am_looking_for",
-            "love_is_for_me",
-            "relation_to_casual_sex",
-            "my_partner_should_be",
-            "marital_status",
-            "children",
-            "relation_to_children",
-            "I_like_movies",
-            "I_like_watching_movie",
-            "I_like_music",
-            "I_mostly_like_listening_to_music",
-            "the_idea_of_good_evening",
-            "I_like_specialties_from_kitchen",
-            "fun",
-            "I_am_going_to_concerts",
-            "my_active_sports",
-            "my_passive_sports",
-            "profession",
-            "I_like_books",
-            "life_style",
-            "music",
-            "cars",
-            "politics",
-            "relationships",
-            "art_culture",
-            "hobbies_interests",
-            "science_technologies",
-            "computers_internet",
-            "education",
-            "sport",
-            "movies",
-            "travelling",
-            "health",
-            "companies_brands",
-            "more",
-            ""
-        ]
-        dfn = pd.read_csv(self.raw_paths[0], sep = "\t", names = node_fields, nrows = None)
-        dfe = pd.read_csv(self.raw_paths[1], sep = "\t", names = ["source", "target"], nrows = None)
+        print("Reading graph from csv file.")
+        # raw_paths[0] will be region_job.csv
+        df_node = pd.read_csv(os.path.join(self.root,"{}.csv".format('region_job')))
+        # raw_paths[1] will be region_job_relationship.txt
+        df_edge = pd.read_csv(self.raw_paths[1], names = ["source", "target"], dtype=int)
 
-        # transform nodes, only keep gender and age
-        dfn = dfn[["gender", "AGE"]]
-        dfn["age"] = dfn["AGE"]
-        dfn = dfn.drop(columns = ["AGE"])
-        dfn = dfn.astype({'gender': 'float', 'age': 'float'})
-
-        # transform edges
-        dfe = dfe.astype({'source': 'str', 'target': 'str'})
-        self.node_frame = dfn
-        self.edge_frame = dfe
+        self.node_frame = df_node
+        self.edge_frame = df_edge
 
         # save as hdf
         store = pd.HDFStore(hdf_path)
-        store["dfn"] = dfn
-        store["dfe"] = dfe
+        store["dfn"] = df_node
+        store["dfe"] = df_edge
         store.close()
         print("Save data frames to 'frames.h5'.")
-        return dfn, dfe
+
+        return df_node, df_edge
 
     def __transform_nodes(self):
         print("Transforming nodes")
         if self.node_frame is None:
-            self.node_frame, self.edge_frame = self.load_frames()
-        # sorting the index does not make sense here
-        self.node_index_map = {str(index): i for i, index in enumerate(self.node_frame.index.unique())}
-        gender_series = self.node_frame["gender"]
-        gender_tensor = torch.zeros(len(gender_series), 2, dtype = torch.float)
-        for i, v in enumerate(gender_series.values):
-            gender_tensor[i, 0 if np.isnan(v) else int(v)] = 1.0
-        age_tensor = torch.tensor(self.node_frame['age'].values, dtype = torch.float).reshape(len(gender_series), -1)
-        x = torch.cat((gender_tensor, age_tensor), dim = -1)  # 1x3 tensor
+            self.read_graph()
+        # index is the user_id here
+        self.node_index_map = {int(user_id): i for i, user_id in enumerate(self.node_frame.index.unique())}
+        # transform nodes: remove the user_id, sensitive attribute and prediction attribute
+        headers = list(self.node_frame.columns)
+        headers.remove("user_id")
+        headers.remove('region')
+        headers.remove("I_am_working_in_field")
+        x = torch.tensor(self.node_frame[headers].values, dtype = torch.float)
         return x, self.node_index_map
 
     def __transform_edges(self):
@@ -183,7 +115,7 @@ class POKEC(InMemoryDataset):
         dst = [self.node_index_map[tgt_id] if tgt_id in self.node_index_map else -1 for tgt_id in self.edge_frame.target]
         edge_index = torch.tensor([src, dst])
 
-        return edge_index, None
+        return edge_index
 
     def create_node_masks(self,d):
         print("Creating classification masks")
@@ -219,11 +151,11 @@ class POKEC(InMemoryDataset):
         d.val_mask = val_mask
 
     def process(self):
-        self.load_frames()
+        self.read_graph()
         nodes_x, nodes_mapping = self.__transform_nodes()
-        edges_index, edges_label = self.__transform_edges()
+        edges_index = self.__transform_edges()
 
-        d = Data(x = nodes_x, edge_index = edges_index, edge_attr = edges_label, y = None)
+        d = Data(x = nodes_x, edge_index = edges_index, edge_attr = None, y = None)
 
         if self.pre_filter is not None:
             d = self.pre_filter(d)
@@ -232,7 +164,7 @@ class POKEC(InMemoryDataset):
             d = self.pre_transform(d)
         POKEC.create_node_masks(d)
         print("Saving data to Pyg file")
-        torch.save(self.collate([d]), self.processed_paths[0])
+        torch.save(self.collate([d]), self.processed_paths[1])
         self.data, self.slices = self.collate([d])
 
     def describe(self):
