f2cec9940102da6354bf820b56535d074b8bc84f
Anmol Anand <aanand@tamu.edu>
2023-02-27 23:49:43 -0600

    shifting paths to argparse

--- examples/auggraph/GraphAug/run_generator.py
+++ examples/auggraph/GraphAug/run_generator.py
@@ -1,14 +1,24 @@
+import argparse
 from examples.auggraph.GraphAug.conf.generator_conf import generator_conf
 from examples.auggraph.GraphAug.conf.reward_gen_conf import reward_gen_conf
 from dig.auggraph.method.GraphAug.runner_generator import RunnerGenerator
 from dig.auggraph.method.GraphAug.constants import *
-from examples.auggraph.GraphAug.conf.paths import *
 
 dataset_name = DatasetName.IMDB_BINARY
 conf = generator_conf[dataset_name]
 conf[REWARD_GEN_PARAMS] = reward_gen_conf[dataset_name][REWARD_GEN_PARAMS]
-conf[REWARD_GEN_STATE_PATH] = get_reward_gen_state_path(dataset_name, conf[REWARD_GEN_PARAMS][MODEL_TYPE],
-                                                        reward_gen_conf[dataset_name][MAX_NUM_EPOCHS] - 1)
+model_type = conf[REWARD_GEN_PARAMS][MODEL_TYPE]
+last_checkpoint = reward_gen_conf[dataset_name][MAX_NUM_EPOCHS] - 1
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_root_path', type=str, default='./dig/auggraph/datasets/tudatasets',
+                    help='The directory with all graph datasets')
+parser.add_argument('--generator_results_path', type=str,
+                    default='./dig/auggraph/method/GraphAug/results/generator_results',
+                    help='The directory where generator states will be stored after each epoch.')
+parser.add_argument('--reward_gen_state_path', type=str,
+                    default='./dig/auggraph/method/GraphAug/results/reward_gen_results' + '/{}/{}/{}.pt'.format(dataset_name.value, model_type.value, str(last_checkpoint).zfill(4)),
+                    help='File path for final training state of reward generation model')
+args = parser.parse_args()
 
-runner = RunnerGenerator(DATA_ROOT_PATH, dataset_name, conf)
-runner.train(GENERATOR_RESULTS_PATH)
+runner = RunnerGenerator(args.data_root_path, dataset_name, conf)
+runner.train(args.generator_results_path)
--- examples/auggraph/GraphAug/run_reward_gen.py
+++ examples/auggraph/GraphAug/run_reward_gen.py
@@ -1,10 +1,16 @@
+import argparse
 from examples.auggraph.GraphAug.conf.reward_gen_conf import reward_gen_conf
 from dig.auggraph.method.GraphAug.runner_reward_gen import RunnerRewardGen
 from dig.auggraph.method.GraphAug.constants import *
-from examples.auggraph.GraphAug.conf.paths import *
 
 dataset_name = DatasetName.IMDB_BINARY
 conf = reward_gen_conf[dataset_name]
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_root_path', type=str, default='./dig/auggraph/datasets/tudatasets',
+                    help='The directory with all graph datasets')
+parser.add_argument('--reward_gen_results_path', type=str, default='./dig/auggraph/method/GraphAug/results/reward_gen_results',
+                    help='The directory where reward gen states will be stored after each epoch.')
+args = parser.parse_args()
 
-runner_reward_gen = RunnerRewardGen(DATA_ROOT_PATH, dataset_name, conf)
-runner_reward_gen.train_test(REWARD_GEN_RESULTS_PATH)
+runner_reward_gen = RunnerRewardGen(args.data_root_path, dataset_name, conf)
+runner_reward_gen.train_test(args.reward_gen_results_path)
